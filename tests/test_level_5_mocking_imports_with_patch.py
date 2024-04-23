baac7b50c9327d4bfd42abf6fe89b6a0f61f0357
Dmitry Smolovich <dsmolovich@gmail.com>
2024-04-22 21:22:11 -0400

    adds patching

--- tests/test_level_5_mocking_imports_with_patch.py
+++ tests/test_level_5_mocking_imports_with_patch.py
@@ -41,3 +41,30 @@ def test_send_with_SlackApi_post_patched(mock_slack_post):
 def test_send_with_SlackApi_object_patched(mock_slack_post):
     send_slack_msg(Mock())
     mock_slack_post.assert_called()
+
+
+class Klass:
+    def __init__(self, a: int):
+        self._a = a
+
+    def sum(self, b: int) -> int:
+        return self._a + b
+
+class AnotherClass:
+    def some_method(self):
+        obj = Klass(a=2)
+        return obj.sum(b=3)
+
+def test_another_class():
+    sut = AnotherClass()
+    assert sut.some_method() == 5
+
+@patch("tests.test_level_5_mocking_imports_with_patch.Klass")
+def test_another_class_with_patched_Klass(some_class):
+    some_class.return_value.sum.return_value = 10
+
+    sut = AnotherClass()
+    assert sut.some_method() == 10
+
+    some_class.assert_called_once_with(a=2)
+    some_class.return_value.sum.assert_called_once_with(b=3)
